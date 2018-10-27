b2846d343bf1155e6b93c5d31fcf7dee4ff52090
bonomo <bonomo@fex.net>
2018-10-27 14:47:43 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/prev.min.css">
+</head>
+<body>
+	<section class="prev-content">
+		<h1>THE BEST SHOP WORLD OF WARCRAFT</h1>
+		<h2 id="adv">. . .</h2>
+		<div class="idea" onclick="location.href = '/shop';">
+			<svg viewBox="0 0 100 88.5">
+				<path d="M73.5,82.2c0-12.4-4.9-24.3-13.7-33.1c-2.9-2.9-6.2-5.4-9.7-7.4c0,0,0,0,0,0c-10.7,6.2-18.6,16.4-21.8,28.4
+					c-1.1,4-1.6,8.1-1.6,12.1c0,0,0,0,0,0c10.7,6.2,23.5,7.9,35.5,4.7C66.2,85.8,70,84.2,73.5,82.2C73.5,82.2,73.5,82.2,73.5,82.2z"/>
+				<ellipse transform="matrix(0.9781 -0.2079 0.2079 0.9781 -3.0109 7.718)" cx="35.2" cy="18.2" rx="12.4" ry="18.4"/>
+				<ellipse transform="matrix(0.2079 -0.9781 0.9781 0.2079 33.5338 77.7756)" cx="64.8" cy="18.2" rx="18.4" ry="12.4"/>
+				<ellipse transform="matrix(0.5299 -0.848 0.848 0.5299 3.4936 93.937)" cx="86.5" cy="43.8" rx="16.5" ry="12.1"/>
+				<ellipse transform="matrix(0.848 -0.5299 0.5299 0.848 -21.1652 13.8225)" cx="13.5" cy="43.8" rx="12.1" ry="16.5"/>
+			</svg>
+		</div>
+	</section>
+
+	<script
+  src="https://code.jquery.com/jquery-3.3.1.min.js"
+  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+  crossorigin="anonymous"></script>
+	<script src="js/app.min.js"></script>
+</body>
+</html>
---
+++ warmane-gold/index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+	<link rel="stylesheet" href="../css/main.min.css">
+
+</head>
+<body>
+	<header class="header-site">
+		<div class="container">
+			<div class="row">
+				<div class="lang col-6">
+				<a href=""><span class="en"></span></a>
+				<a href=""><span class="ru"></span></a>
+			</div>
+			<div class="support-status col-6">Support: <span>online</span></div>
+			</div>
+		</div>
+	</header>
+	
+	<div class="menu-site">
+		<div></div>
+	</div>
+
+	<section class="container games">
+		<div class="game">
+			<h1>World of Warcraft Private Servers</h1>
+			<div class="servers">
+				<div class="server col-12 col-lg-6">
+					<div class="row">
+						<div class="logo col-3">
+							<img src="../img/alt/warmane.png" alt="">
+						</div>
+						<div class="options col-9">
+							<h2>Warmane Icecrown / Lordaeron / Outland</h2>
+							<div class="select-type">
+								<a href="" class="button-type">Gold</a>
+								<a href="" class="button-type">Coins</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="server col-12 col-lg-6">
+					<div class="row">
+						<div class="logo col-3">
+							<img src="../img/alt/warmane.png" alt="">
+						</div>
+						<div class="options col-9">
+							<h2>Warmane Icecrown / Lordaeron / Outland</h2>
+							<div class="select-type">
+								<a href="" class="button-type">Gold</a>
+								<a href="" class="button-type">Coins</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				
+
+			</div>
+		</div>
+	</section>		
+
+	<div class="specials-full">
+		<div class="specials container">
+			<div class="row">
+				<h1>Our advantages</h1>
+			</div>
+			<div class="row">
+				<div class="special col-12 col-sm-4">
+						<span><i class="fa fa-fighter-jet"></i></span>
+						<h2>Fast Delivery</h2>
+				</div>
+				<div class="special col-12 col-sm-4">
+						<span><i class="fa fa-money"></i></span>
+						<h2>Best Price</h2>
+				</div>
+				<div class="special col-12 col-sm-4">
+						<span><i class="fa fa-paw"></i></span>
+						<h2>100% Safe</h2>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<footer class="footer">
+		<div class="container">
+			<p>With <span style="color: red;">❤</span> by Bonomo Shop</p>
+		</div>
+	</footer>
+	<script
+  src="https://code.jquery.com/jquery-3.3.1.min.js"
+  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+  crossorigin="anonymous"></script>
+	<script src="../js/app.min.js"></script>
+</body>
+</html>
